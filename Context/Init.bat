92faee07b41dcb1a8da7bc60af5e8412ee1448f0
Yatima Santamorena <yatima@santamorena.me>
2021-09-02 20:18:59 +0200

    add context for images

---
+++ Context/Init.bat
@@ -0,0 +1,13 @@
+@echo off
+
+echo +-------------------------------------------+
+echo ^| Welcome to Yatima's Windows container! ♥  ^|
+echo +-------------------------------------------+
+echo(
+echo vCores: %NUMBER_OF_PROCESSORS%
+systeminfo | findstr "Available" | findstr "Physical" | findstr "Memory"
+@REM echo vDisks:
+@REM wmic diskdrive get Name,Model,SerialNumber,Size,Status
+echo(
+ver
+echo(
